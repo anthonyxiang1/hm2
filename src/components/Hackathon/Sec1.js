05c7fb5a23988d74143933d48ae377a780235e24
LiangGeorge <geoliang01@gmail.com>
2019-08-16 15:37:30 -0400

    Add changes and card

--- src/components/Hackathon/Sec1.js
+++ src/components/Hackathon/Sec1.js
@@ -2,38 +2,47 @@ import React from 'react';
 import { Container, Col, Button, Image, Row, ButtonToolbar } from 'react-bootstrap';
 import UserCard from './UserCard'
 import TeamCard from './TeamCard'
+import HackathonCard from '../Home/HackathonCard'
 class Sec1 extends React.Component {
 
     render() {
         return (
             <div className="sec1">
                 <div className="rectangle" ></div>
-                <Container className="flexbox" >
+
+                <Container>
                     <Row>
-                        <Col id="namebox" className="namebox" >StuyHacks</Col>
+                        <Col id="namebox " className="namebox col-12" >StuyHacks</Col>
                     </Row>
+                </Container> 
+
+                <Container className="flexbox" >
+                    
                     <Row >
-                        <Col id="date" className="date">DateDateDate</Col>
-                        <Col>
+                         <Col xs={{span:12}} sm={{span:12}} md={{span:4, order:2}} lg={{span:4, order:2}} xl={{span:4, order:2}}>
                             <img id="hackathonImg"
                                 src={require("./stuyhacks.png")}
                                 className="hackathonImg">
                             </img>
                         </Col>
-                        <Col id="location" className="location">LocationLocationLocation</Col>
+                        <Col id="date" xs={{span:12}} sm={{span:12}} md={{span:4, order:1}} lg={{span:4, order:1}} xl={{span:4, order:1}} className="date">DateDateDate</Col>
+                        <Col id="location" xs={{span:12}} sm={{span:12}} md={{span:4, order:3}} lg={{span:4, order:3}} xl={{span:4, order:3}} className="location">LocationLocationLocation</Col>
                     </Row>
                 </Container>
+
+
                 <Container className="addbtn" >
                     <Row className="justify-content-center addtxt">
                         <Col>
                             <center>
-                                <h1>Add Yourself To This Matching Pool!</h1>
+                                <h1 id="addyourself">Add Yourself To This Matching Pool!</h1>
                             </center>
                         </Col>
                     </Row>
                     <Row className="justify-content-center">
                         <Col>
                             <img
+                                id="downarr"
                                 src={require("./downarrow.png")}
                                 alt="clickme"
                                 className="downarr">
@@ -45,22 +54,21 @@ class Sec1 extends React.Component {
                     <Row className="justify-content-center">
                         <Col>
                             <center>
-                                <Button className="addme" variant="primary" size="lg" block onClick={this.myFunction}>I'm Going!</Button>
+                                <Button type="button" id="addme" className="addme" variant="primary" size="lg" onClick={this.myFunction}>I'm Going!</Button>
                             </center>
                         </Col>
                     </Row>
                 </Container>
                 <ButtonToolbar className="btn-duo justify-content-center">
-                    <Button id="matchme" className="matchme" variant="secondary" style={{ display: "none" }} onClick={this.showAvailable}>Match Me!</Button>
-                    <Button id="alreadymatched" className="alreadymatched" variant="success" style={{ display: "none" }}onClick={this.showAvailable}>I Already Have A Team!</Button>
+                    <Button type="button" id="matchme" className="matchme" variant="secondary" style={{ display: "none" }} onClick={this.showAvailable}>Match Me!</Button>
+                    <Button type="button" id="alreadymatched" className="alreadymatched" variant="success" style={{ display: "none" }}onClick={this.showAvailable}>I Already Have A Team!</Button>
                 </ButtonToolbar>
 
 
                 <center className="addedtxt" id="addedtxt" style={{ display: "none" }}>
                     <h1>You Have Been Added!</h1>
                 </center>
-
-
+                
                 <Container fluid={true} id="matchedUsers" className="matchedUsers" style={{ display: "none" }}>
                     <Row>
                         <Col>
@@ -104,6 +112,7 @@ class Sec1 extends React.Component {
                     <Row>
                         {/* This is here to show how to move cards to a side */}
                         <Col>
+                            <HackathonCard/>
                         </Col>
                         <Col>
                             <TeamCard/>
@@ -114,26 +123,40 @@ class Sec1 extends React.Component {
                 
                     
                 </Container>
-
+                <div className="pgbottom">
+                </div>
             </div>
-
+            
         );
+        
     }
     myFunction() {
-        $("#matchme").fadeIn("slow")
-        $("#alreadymatched").fadeIn("slow")
+        $("#downarr").fadeOut("slow")
+        $("#addme").fadeOut("slow")
+        setTimeout(function(){
+        $("#addedtxt").fadeIn()
         //document.getElementById("matchme").style.visibility = "visible";
         //document.getElementById("alreadymatched").style.visibility = "visible";
+        },1250);
+        $("#matchme").fadeIn("slow")
+        $("#alreadymatched").fadeIn("slow")
+        $('html, body').animate({
+            scrollTop: $("#matchme").offset().top
+        }, 3000);
     }
     showAvailable(){
-        $("#addedtxt").fadeIn()
+        $("#addyourself").fadeOut("slow")
+        
+        $("#alreadymatched").fadeOut("slow")
+        $("#matchme").fadeOut("slow")
         setTimeout(function(){
         $("#matchedUsers").fadeIn("slow")
-        
-        $('html, body').animate({
-            scrollTop: $("#matchedUsers").offset().top
-        }, 2000);
-        },1250);
+        setTimeout(function(){
+            $('html, body').animate({
+                scrollTop: $("#matchedUsers").offset().top
+            }, 2000);
+        },1250)
+        },2500);
     }
 }
 
